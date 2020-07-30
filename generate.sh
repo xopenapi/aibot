36ab549099ebdd1a7d3a46fc61273d6be1281fed
newtalentxp <52388483@qq.com>
2020-07-30 19:19:37 +0800

    fix

--- generate.sh
+++ generate.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-    -i /local/aibot.yaml \
+    -i /local/tanyibot.yaml \
     --git-repo-id aibot \
     --git-user-id xopenapi \
     --package-name aibot \
     -g go \
-    -o /local/out/aibot-api
+    -o /local/out/tanyibot-api
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-    -i /local/aibot.yaml \
-    --git-repo-id aibot-api-server \
+    -i /local/tanyibot.yaml \
+    --git-repo-id tanyibot-api-server \
     --git-user-id xopenapi \
     -g kotlin-spring \
-    -o /local/out/aibot-api-server
+    -o /local/out/tanyibot-api-server
