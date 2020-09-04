9a7e532b330a38d94b6af1e0cb00b594f78980a3
许盛 <fioman@qq.com>
2020-09-05 00:55:47 +0800

    fix

--- generate-tanyibot.sh
+++ generate-tanyibot.sh
@@ -3,6 +3,6 @@ docker run --rm -v "${PWD}:/local" xopenapi/openapi-generator-cli generate \
     -i /local/tanyibot.yaml \
     --git-repo-id tanyibot \
     --git-user-id xopenapi \
-    --package-name tanyibot \
+    --package-name tanyibot-api-go \
     -g go \
     -o /local/out/tanyibot-api-go
